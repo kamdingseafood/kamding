7ff805dd1f573ebcd49583dd74bbb49fea7ecd6d
vincewu124 <vincewu124@hotmail.com>
2020-12-22 16:33:19 -0800

    responsive website

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,4 @@
-$(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
-});
-
-document.querySelector('#menu-button').addEventListener('click', openMenu);
+document.querySelector('#cta-menu').addEventListener('click', openMenu);
 
 function openMenu(){
     window.open('kamdingmenu.pdf');
